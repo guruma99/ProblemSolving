80ea3711100522f9f1602b4f1c21d933a9001db4
Haemin Kang <bowbow_ovo@naver.com>
2025-06-13 23:45:59 +0900

    [level 0] Title: 조건 문자열, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,14 @@
+function solution(ineq, eq, n, m) {
+    switch (ineq + eq) {
+        case ">=":
+            return n >= m ? 1 : 0;
+        case "<=":
+            return n <= m ? 1 : 0;
+        case ">!":
+            return n > m ? 1 : 0;
+        case "<!":
+            return n < m ? 1 : 0;
+        default:
+            return 0;
+    }
+}
